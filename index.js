dd4922f23fe057569e099c168b161a99230b113a
Nick Lozenski <nicholas.lozenski@gmail.com>
2018-04-06 04:44:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function subtract(a,b){
 }
 function multiply(a,b) { return a *= b}
 function divide(a,b){return a/=b}
-function inc(n){return n+1}
-function dec(n){return n-1}
+function inc(n){return ++n}
+function dec(n){return --n}
 
